301b47ec432836d41b7c426c3055671be09aa70c
theKelvinCode <thekelvincode@gmail.com>
2025-07-25 10:04:40 +0100

    licensing & org service: added profile and updated some things like ports and other minor things.

--- src/main/java/com/optimagrowth/license/expats/organization/dto/Organization.java
+++ src/main/java/com/optimagrowth/license/expats/organization/dto/Organization.java
@@ -1,4 +1,22 @@
 package com.optimagrowth.license.expats.organization.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class Organization {
+
+    UUID id;
+
+    String name;
+
+    String contactName;
+
+    String contactEmail;
+
+    String contactPhone;
 }
